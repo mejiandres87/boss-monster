cdfa4fef605432e02cd4d81deabfd791cb09509f
Andrés Felipe Mejía <mejia.andres87@gmail.com>
2020-08-29 11:11:59 -0500

    first commit

---
+++ src/main/java/com/pris/bossmonster/BossMonsterApplication.java
@@ -0,0 +1,13 @@
+package com.pris.bossmonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossMonsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossMonsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pris/bossmonster/BossMonsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pris.bossmonster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossMonsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
